480567e7fd2d87c33b789c790014655c87852a73
CapiCode <prodcapicode@gmail.com>
2019-11-11 14:49:18 +0100

    Update about section

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var options = {
-    strings: ["développement web"],
-    typeSpeed: 100,
-    backDelay: 5000,
-    backSpeed: 120,
-    loop: false
-  };
-  var Typed = new Typed("#row", options);
+  strings: ["web"],
+  typeSpeed: 100,
+  backDelay: 5000,
+  backSpeed: 120,
+  loop: true
+};
+var Typed = new Typed("#row", options);
