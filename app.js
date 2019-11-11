739547c2d4f6d6cba60bc07cabaecca27e658971
CapiCode <prodcapicode@gmail.com>
2019-11-11 02:18:15 +0100

    Add about folder

---
+++ app.js
@@ -0,0 +1,8 @@
+var options = {
+    strings: ["développement web"],
+    typeSpeed: 100,
+    backDelay: 5000,
+    backSpeed: 120,
+    loop: false
+  };
+  var Typed = new Typed("#row", options);
