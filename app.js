bbc872f772956b813bc3d0f8661e64589b342eae
CapiCode <prodcapicode@gmail.com>
2020-01-14 22:28:52 +0100

    Add new navigation

--- app.js
+++ app.js
@@ -1,24 +1,56 @@
-$(function() {
-
-  var PreviousScrollPosition = 0;
-  $(window).scroll(function() {
-      CurrentScrollPosition = $(window).scrollTop();
-      if(PreviousScrollPosition > CurrentScrollPosition) {
-          $('nav').removeClass('hide');
-      } else {
-          $('nav').addClass('hide');
-      }
-      PreviousScrollPosition = CurrentScrollPosition;
-  });
-
+var t1 = new TimelineMax({
+  paused: true
 });
 
-$(function () {
-  $("#logo-home").click(function () {
-    $("html, body").animate({
-      scrollTop: $("section.home").offset().top
-    }, 1000);
-  });
+t1.to(".one", 0.8, {
+  y: 6,
+  rotation: 45,
+  ease: Expo.easeInOut
+});
+t1.to(".two", 0.8, {
+  y: -6,
+  rotation: -45,
+  ease: Expo.easeInOut,
+  delay: -0.8
+});
+
+t1.to(".menu", 2, {
+  top: "0%",
+  ease: Expo.easeInOut,
+  delay: -2
+});
+
+t1.staggerFrom(".menu ul li", 2, {
+  x: -200,
+  opacity: 0,
+  ease: Expo.easeOut
+}, 0.3);
+
+// t1.reverse();
+// $(document).on("click", ".toggle-btn", function () {
+//   t1.reversed(!t1.reversed());
+//   document.getElementById('hero-text').classList.remove('is-visible');
+//   document.getElementById('icon-arrow').classList.remove('is-visible');
+// });
+
+t1.reverse();
+$(document).on("click", ".toggle-btn", function () {
+  t1.reversed(!t1.reversed());
+  document.getElementById('hero-text').classList.add('is-visible');
+  document.getElementById('icon-arrow').classList.add('is-visible');
+});
+
+$(document).on("click", "a", function () {
+  t1.reversed(!t1.reversed());
+});
+
+
+// $(function () {
+//   $("#logo-home").click(function () {
+//     $("html, body").animate({
+//       scrollTop: $("section.home").offset().top
+//     }, 1000);
+//   });
   $("#link-about").click(function () {
     $("html, body").animate({
       scrollTop: $("section.about").offset().top
@@ -49,7 +81,7 @@ $(function () {
       scrollTop: $("section.project").offset().top
     }, 1000);
   });
-});
+
 
 
 const backToTopButton = document.querySelector("#btn-top");
@@ -107,11 +139,7 @@ var sr = ScrollReveal({
 
 
 // section hero
-sr.reveal('nav', {
-  afterReveal: function (domEl) {
-    domEl.style.letterSpacing = '1px';
-  }
-});
+
 sr.reveal('.hero-text h1', {
   delay: 500
 });
