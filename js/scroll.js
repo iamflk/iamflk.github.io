dde14532fb44b6e8e13de19fe7463132a3b57451
CapiCode <prodcapicode@gmail.com>
2020-07-08 23:43:41 +0200

    Update scroll link navigation

--- js/scroll.js
+++ js/scroll.js
@@ -7,109 +7,26 @@ window.addEventListener('scroll', function(){
   
   // SCROLL LINK NAV
   
-  
-  $("#link-home").click(function () {
-    $("html, body").animate({
-      scrollTop: $(".home").offset().top
-    }, 1000);
-  });
-
   document.querySelector('#link-home').addEventListener('click', () =>{
-    document.querySelector('html, body').classList.add('animate');
-    offset(document.querySelector('.home')).top;
-    setTimeout('', 1000);
-  })
-  
-  // document.querySelector('#link-home').addEventListener('click', () =>{
-  //   document.querySelector('html, body').scrollIntoView({ behavior: 'smooth'});
-  // })
-
-
-
-    document.querySelector('#link-contact').addEventListener('click', () =>{
-      document.querySelector('html, body').scrollIntoView({ behavior: 'smooth'});
+    document.querySelector('html, body').scrollIntoView({ behavior: 'smooth'});
   })
 
-  document.addEventListener('click', (event) => {
-    if (!event.target.matches('.btn-scroll-into')) return;
-  
-    event.preventDefault();
-  
-    var element = document.getElementById(event.target.dataset.target);
-  
-    element.scrollIntoView();
+  const links = document.querySelectorAll(".navigation__items a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
   });
-
-
-  // document.querySelector('#link-home').addEventListener('click', () =>{
-  //   document.querySelector('html, body').scrollIntoView({ behavior: 'smooth', block: 'end'});
-  // })
-
-  // $("#link-about").click(function () {
-    //   $("html, body").animate({
-    //     scrollTop: $("section.about").offset().top
-    //   }, 1000);
-    // });
-
-  // document.querySelector('#link-about').addEventListener('click', () =>{
-  //   document.querySelector('section#about').scrollIntoView({ behavior: 'smooth'});
-  //   document.querySelector('html, body').classList.add('animate');
-  // });
-
-    // document.querySelector('html, body').scrollIntoView({ behavior: 'smooth', block: 'end'});
-
-
-  // document.querySelector('#link-project').addEventListener('click', () =>{
-  //   document.querySelector('html, body').scrollIntoView({ behavior: 'smooth', block: 'start'});
-  // })
-
-  // document.querySelector('#link-skill').addEventListener('click', () =>{
-  //   document.querySelector('html, body').scrollIntoView({ behavior: 'smooth', block: 'end'});
-  // })
-
-
-  // document.querySelector('#link-contact').addEventListener('click', () =>{
-  //   document.querySelector('html, body').scrollIntoView({ behavior: 'smooth', block: 'end'});
-  // })
-
-  // document.querySelector('#arrow').addEventListener('click', () =>{
-  //   document.querySelector('html, body').scrollIntoView({ behavior: 'smooth', block: 'end'});
-  // })
-
-  // document.querySelector('#see-project').addEventListener('click', () =>{
-  //   document.querySelector('html, body').scrollIntoView({ behavior: 'smooth', block: 'end'});
-  // })
-  
-    // $("#link-about").click(function () {
-    //   $("html, body").animate({
-    //     scrollTop: $("section.about").offset().top
-    //   }, 1000);
-    // });
-    // $("#link-skill").click(function () {
-    //   $("html, body").animate({
-    //     scrollTop: $("section.skill").offset().top
-    //   }, 1000);
-    // });
-    // $("#link-project").click(function () {
-    //   $("html, body").animate({
-    //     scrollTop: $("section.project").offset().top
-    //   }, 1000);
-    // });
-    // $("#link-contact").click(function () {
-    //   $("html, body").animate({
-    //     scrollTop: $("footer").offset().top
-    //   }, 1000);
-    // });
-    // $("#arrow").click(function () {
-    //   $("html, body").animate({
-    //     scrollTop: $("section.about").offset().top
-    //   }, 1000);
-    // });
-    // $("#see-project").click(function () {
-    //   $("html, body").animate({
-    //     scrollTop: $("section.project").offset().top
-    //   }, 1000);
-    // });
+}
 
     // BUTTON SCROLL TOP
 const backToTopButton = document.querySelector("#btn-top");
@@ -159,44 +76,3 @@ function easeInOutCubic(t, b, c, d) {
   t -= 2;
   return c / 2 * (t * t * t + 2) + b;
 };
-
-
-/*   ---------------------------------- */
-
-// function smoothScroll(target, duration){
-//   var target = document.querySelector(target);
-//   var targetPosition = target.getBoundingClientRect().top;
-//   var startPosition = window.pageYOffset;
-//   var distance = targetPosition - startPosition;
-//   var startTime = null;
-
-
-//     function animation(currentTime){
-//       if(startTime === null) startTime = currentTime;
-//       var timeElapsed = currentTime - startTime;
-//       var run = ease(timeElapsed, startPosition, distance, duration);
-//       window.scrollTo(0, run);
-
-//       if(timeElapsed < duration) requestAnimationFrame(animation);
-//     }
-
-//     function ease(t, b, c, d){
-//       t /= d / 2;
-//       if (t < 1) return c / 2 * t * t + b;
-//       t--;
-//       return -c / 2 * (t * (t - 2) - 1) + b;
-//     }
-
-//     requestAnimationFrame(animation);
-// }
-
-// var home =  document.querySelector('.home');
-// var about =  document.querySelector('#skill');
-
-// home.addEventListener('click', () => {
-//   smoothScroll('.home', 1000);
-// })
-
-// about.addEventListener('click', () => {
-//   smoothScroll('#about', 1000);
-// });
